3e969e01c12a0aad122c76fdf32ae14f53d106a5
luispaezlemon <luispaez.lemon@gmail.com>
2017-07-26 00:22:39 -0300

    fix en ""

--- server/index.js
+++ server/index.js
@@ -6,5 +6,5 @@ app.use('/presentacion', express.static('presentacion'));
 app.use(express.static('web'));
 
 app.listen(port, function () {
-  console.log('Example app listening on port "+ port)
+  console.log("Example app listening on port "+ port)
 });
